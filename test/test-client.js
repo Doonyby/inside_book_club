bae77b5372d28aa428278973944fa46bd2bd0d87
Dan Riding <doonyby@gmail.com>
2016-12-03 23:22:26 -0700

    added travis

---
+++ test/test-client.js
@@ -0,0 +1,6 @@
+describe('Inside Book Club Client', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+});
---
+++ test/test-server.js
@@ -0,0 +1,44 @@
+global.DATABASE_URL = 'mongodb://localhost/inside_book_club-test';
+
+import chai from ('chai');
+import chaiHttp from ('chai-http');
+import server from ('../server/index.js');
+
+const should = chai.should();
+const app = server.app;
+//const storage = server.storage;
+//const Item = require('../models/item');
+
+chai.use(chaiHttp);
+
+describe('Inside Book Club Server', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+});
+
+// describe('Shopping List', function() {
+//     before(function(done) {
+//         server.runServer(function() {
+//             Item.create({name: 'Broad beans'},
+//                         {name: 'Tomatoes'},
+//                         {name: 'Peppers'}, function() {
+//                 done();
+//             });
+//         });
+//     });
+//     it('should list items on GET', function(done) {
+//         chai.request(app)
+//             .get('/items')
+//             .end(function(err, res) {
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+//     after(function(done) {
+//         Item.remove(function() {
+//             done();
+//         });
+//     });
+// });

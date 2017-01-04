e04e6a133581b05c7eef089bf852e9539684d9b8
Dan Riding <doonyby@gmail.com>
2017-01-03 17:04:12 -0700

    added mlab, travis-heroku deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 
 						global.DATABASE_URL ||
 						(process.env.NODE_ENV === 'production' ?
-							'mongodb://localhost/iside_book_club' :
+							'mongodb://heroku_nb6plj6g:guj2a2crviqj5ioqpmboufrt0f@ds151108.mlab.com:51108/heroku_nb6plj6g' :
 							'mongodb://localhost/inside_book_club-dev');
 exports.PORT = process.env.PORT || 8080;

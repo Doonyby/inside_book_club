ec7850cde94a2d9b1aa6e11355fd54672504cdd0
Dan Riding <doonyby@gmail.com>
2017-01-28 15:42:52 -0700

    built production1

---
+++ webpack.config.prod.js
@@ -0,0 +1,48 @@
+const path = require('path')  
+const webpack = require('webpack')
+
+export default {  
+  devtool: 'source-map',
+
+  entry: [
+    './client/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'API_HOST': 'https://mighty-anchorage-70654.herokuapp.com/'
+      }
+    })
+  ],
+
+  module: {
+    preLoaders: [{test: /\.json$/, loader: 'json'}],
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel',
+        exclude: /node_modules/ },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'client/css') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  }
+}

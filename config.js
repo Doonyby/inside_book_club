7915b0b025a608626e39023513f893b04f0a9939
Dan Riding <doonyby@gmail.com>
2017-02-03 15:50:25 -0700

    deleted old heroku app and created new to see if signup works

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 
 						global.DATABASE_URL ||
 						(process.env.NODE_ENV === 'production' ?
-							'mongodb://heroku_nb6plj6g:guj2a2crviqj5ioqpmboufrt0f@ds151108.mlab.com:51108/heroku_nb6plj6g' :
+							'mongodb://heroku_xbqsvps2:6a94vd4ljbjn52367e33m4f2vs@ds139979.mlab.com:39979/heroku_xbqsvps2' :
 							'mongodb://localhost/inside_book_club-dev');
 exports.PORT = process.env.PORT || 8080;

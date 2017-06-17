62c993d65a440aaf565b5c2514a3ecb80d87b2f3
Dan Riding <doonyby@gmail.com>
2017-06-16 23:48:35 -0600

    added logic to dropdown

--- client/components/user/Navigation.js
+++ client/components/user/Navigation.js
@@ -7,14 +7,26 @@ import { LinkContainer } from "react-router-bootstrap";
 
 class Navigation extends React.Component {
 	render () {
-		let dropdown = {
+		let menuDropdown = {
 			display: "none",
+		}
+		let exitDropdown = {
+			display: "none",
+		}
+		let changeDropdown = (dropdownType) => {
+			if (dropdownType.display === "none") {
+				dropdownType.display = "block"; 
+			} else {
+				dropdownType.display = "none";	
+			}
+			
+			console.log(dropdownType);			
 		}
 		return (
 			<div className="navBar">
 				<ul>
-			        <li className="navLeft" id="menuDropdown">Club Menu</li>
-				        <div style={dropdown}>
+			        <li className="navLeft" onClick={ () => { changeDropdown(menuDropdown)} }>Club Menu</li>
+				        <div style={menuDropdown}>
 				        	<LinkContainer to={{ pathname: '/home' }}>
 				          		<MenuItem eventKey={1.1}>My Club</MenuItem>
 				        	</LinkContainer>
@@ -27,8 +39,8 @@ class Navigation extends React.Component {
 							</LinkContainer>			        
 				        </div>
 				    <li className="navLeft navTitle">Inside Book Club</li>
-			        <li className="navRight">{this.props.club.homeReducer.name.toUpperCase()}</li>
-				        <div style={dropdown}>
+			        <li className="navRight" onClick={ () => { changeDropdown(exitDropdown)} }>{this.props.club.homeReducer.name.toUpperCase()}</li>
+				        <div style={exitDropdown}>
 			        		<LinkContainer to={{ pathname: '/' }}>
 				          		<MenuItem eventKey={2.1}>Sign out</MenuItem>
 							</LinkContainer>
